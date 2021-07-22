c7a3f66213fe9ff8c7bb25c78f5112edfa5725ae
vishal9876 <vishal.c.addweb@gmail.com>
2021-07-22 12:01:31 +0530

    builder plugins

---
+++ updates/builder_table_create_creator_movies_.php
@@ -0,0 +1,24 @@
+<?php namespace Creator\Movies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCreatorMovies extends Migration
+{
+    public function up()
+    {
+        Schema::create('creator_movies_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('year')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('creator_movies_');
+    }
+}
---
+++ updates/builder_table_update_creator_movies_.php
@@ -0,0 +1,23 @@
+<?php namespace Creator\Movies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateCreatorMovies extends Migration
+{
+    public function up()
+    {
+        Schema::table('creator_movies_', function($table)
+        {
+            $table->string('slug')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('creator_movies_', function($table)
+        {
+            $table->dropColumn('slug');
+        });
+    }
+}
